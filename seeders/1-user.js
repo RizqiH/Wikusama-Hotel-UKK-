1071783f2dcf5fa1e09cf94dfa596e8586c552d9
RizqiH <rizkiamanan@gmail.com>
2023-12-24 13:10:25 +0700

    first commit

---
+++ seeders/1-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const md5 = require('md5');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('user', [{
+      nama_user: 'admin_nopal',
+      foto: null,
+      email: 'admin_nopal@wikusamahotel.com',
+      password: md5('admin'),
+      role: 'admin',
+    }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete('user', null, {});
+  }
+};
